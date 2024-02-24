92b206e28efb034112692af0432a3d557786c17c
EmtecAmik <120498388+EmtecAmik@users.noreply.github.com>
2024-02-24 15:55:24 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,103 @@
+// 1. Создать кнопку и красный квадрат с размерами 200х200px. При клике на кнопку менять у квадрата цвет заднего фона на зеленый и уменьшать его до размеров 100х100px.
+
+//      const squ = document.querySelector('.square')
+//      const btn = document.querySelector('.btn')
+
+   
+//      btn.addEventListener("click", function() {
+//          squ.style.backgroundColor = "green"
+//          squ.style.width = "100px"
+//          squ.style.height = "100px"
+     
+//       })
+
+
+
+// 2. Создать кнопку и розовый квадрат с размерами 200х200px. При клике на кнопку менять цвет на синий и выводить в консоль обновленный цвет квадрата.
+
+
+    // const squ = document.querySelector('.square')
+    // const btn = document.querySelector('.btn')
+
+   
+    // btn.addEventListener("click", function() {
+    //     squ.style.backgroundColor = "blue"
+    //     console.log(squ.style.backgroundColor = "blue")
+    // })
+
+
+
+
+// 3. Создать кнопку и квадрат с размерами 150х150px. При клике на кнопку увеличивать высоту и ширину квадрата на 20px
+
+
+    // const squ = document.querySelector('.square')
+    // const btn = document.querySelector('.btn')
+  
+
+    //     btn.addEventListener("click", function()
+    //      {
+    //         let currentWidth = squ.offsetWidth
+    //         let currentHeight = squ.offsetHeight
+        
+           
+    //         let newWidth = currentWidth + 20
+    //         let newHeight = currentHeight + 20
+        
+            
+    //         squ.style.width = newWidth + "px"
+    //         squ.style.height = newHeight + "px"
+
+    //     })
+     
+
+
+
+// 4. Создать кнопку и div с классом root. При клике на кнопку в div создается параграф синего цвета. Текст параграфа произвольный.
+
+    // const container = document.querySelector('.root')
+    // const btn = document.querySelector('.btn')
+
+    // btn.addEventListener("click", function()
+    //       {
+    //         const parag = document.createElement('p')
+    //         parag.textContent = 'lorem asd mkd lf'
+    //         parag.style.color = 'blue'
+    //         container.appendChild(parag)
+    //      })
+    
+
+
+
+ // 5.    Создать кнопку и div с классом root. При клике на кнопку в div создаются по очереди параграфы синего и зеленого цветов. Первый цвет синий.
+
+
+    // const container = document.querySelector('.root');
+    //  const btn = document.querySelector('.btn');
+    //  let clickCount = 1
+
+    //  btn.addEventListener("click", function()
+    //        {  
+    //         let parag = document.createElement('p')
+
+
+    //          if (clickCount === 1 ) {
+                
+    //          parag.textContent = 'lorem asd mkd lf';
+    //         parag.style.color = 'blue';
+
+    //          }
+
+    //          else {
+                
+    //          parag.textContent = 'lorem asd mkd lf';
+    //          parag.style.color = 'green';
+
+    //          }
+
+    //          clickCount++;
+
+
+             
+    //         container.appendChild(parag);
+    //       })
